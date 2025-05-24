75ac09039435dda1f2fa39e1484ca3203a5d928e
Dasha Sermiazhko <dasha.sermiazhko@gmail.com>
2025-05-24 18:03:45 +0300

    add s word

--- World_books/WebBooks/WebBooks/settings.py
+++ World_books/WebBooks/WebBooks/settings.py
@@ -13,7 +13,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 from .tok import token
 import os
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
+# Builds paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
